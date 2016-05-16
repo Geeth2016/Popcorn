a2fb81da06f214526adf2a491469b4203dd87d72
Geeth <noreply>
2016-05-16 19:41:21 +0000

    Add module UI

---
+++ u-i.js
@@ -0,0 +1,15 @@
+/**
+ * @module //u-i
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class UI
+ * @extends Montage
+ */
+exports.UI = Montage.specialize(/** @lends UI# */ {
+    constructor: {
+        value: function UI() {
+            this.super();
+        }
+    }
+});
